bf9d99a1671d25746e8933067efd370f1e02841b
Lênin Müller Sampaio <lenin_muller@uol.com.br>
2026-06-01 14:42:25 -0300

    fix: usa payload 2.0 na api auth

--- infra/main.tf
+++ infra/main.tf
@@ -182,6 +182,7 @@ resource "aws_apigatewayv2_integration" "lambda_integration" {
   integration_type   = "AWS_PROXY"
   integration_uri    = aws_lambda_alias.auth_api_live.invoke_arn
   integration_method = "POST"
+  payload_format_version = "2.0"
 }
 
 resource "aws_apigatewayv2_authorizer" "jwt" {
